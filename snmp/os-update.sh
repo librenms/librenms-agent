fbbb594163a6ffe5dc3faff333b2c29c9280b1a8
crcro <crc@nuamchefazi.ro>
2016-09-16 17:08:07 +0300

    reverted back to os-release checks, added arch pacman

---
+++ snmp/os-update.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+################################################################
+# copy this script to /etc/snmp/ and make it executable:       #
+# chmod +x /etc/snmp/os-updates.sh                             #
+# ------------------------------------------------------------ #
+# edit your snmpd.conf and include:                            #
+# extend osupdate /opt/os-updates.sh 			       #
+#--------------------------------------------------------------#
+# restart snmpd and activate the app for desired host          #
+#--------------------------------------------------------------#
+# please make sure you have the path/binaries below            #
+################################################################ 
+BIN_AWK='/usr/bin/awk'
+BIN_WC='/usr/bin/wc'
+CMD_WC='-l'
+BIN_ZYPPER='/usr/bin/zypper'
+CMD_ZYPPER='lu'
+BIN_YUM='/usr/bin/yum'
+CMD_YUM='-q check-update'
+BIN_APT='/usr/bin/apt-get'
+CMD_APT='-s upgrade'
+BIN_PACMAN='/usr/bin/pacman'
+CMD_PACMAN='-Sup'
+
+################################################################
+# Don't change anything unless you know what are you doing     #
+################################################################
+if [ -f /etc/os-release ]; then
+	OS=`$BIN_AWK -F= '/^ID=/{print $2}' /etc/os-release`
+	if [ $OS == "opensuse" ]; then
+		UPDATES=`$BIN_ZYPPER $CMD_ZYPPER | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 3 ]; then
+			echo $(($UPDATES-3));
+		else
+			echo "0";
+		fi
+	elif [ $OS == "\"centos\"" ]; then
+		UPDATES=`$BIN_YUM $CMD_YUM | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 6 ]; then
+			echo $(($UPDATES-6));
+		else
+			echo "0";
+		fi
+	elif [ $OS == "ubuntu" ]; then
+		UPDATES=`$BIN_APT $CMD_APT | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 1 ]; then
+			echo $(($UPDATES-1));
+		else
+				echo "0";
+		fi
+	elif [ $OS == "arch" ]; then
+		UPDATES=`$BIN_PACMAN $CMD_PACMAN | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 1 ]; then
+        		echo $(($UPDATES-1));
+    		else
+        		echo "0";
+		fi
+	fi
+else
+	echo "0";
+fi
