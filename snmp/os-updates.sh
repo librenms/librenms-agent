58e16b794a0e33d0dd71d8c1f936bc8b25ad7ced
crcro <crc@nuamchefazi.ro>
2016-09-25 16:28:37 +0300

    snmp-extend: os-updates (#65)
    * reverted back to os-release checks, added arch pacman
    
    * fixed file name

--- snmp/os-updates.sh
+++ snmp/os-updates.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 ################################################################
-# copy this script to somewhere like /opt and make chmod +x it #
-# edit your snmpd.conf and include                             #
-# extend osupdate /opt/os-updates.sh                           #
+# copy this script to /etc/snmp/ and make it executable:       #
+# chmod +x /etc/snmp/os-updates.sh                             #
+# ------------------------------------------------------------ #
+# edit your snmpd.conf and include:                            #
+# extend osupdate /opt/os-updates.sh 			       #
+#--------------------------------------------------------------#
 # restart snmpd and activate the app for desired host          #
+#--------------------------------------------------------------#
+# please make sure you have the path/binaries below            #
 ################################################################ 
+BIN_AWK='/usr/bin/awk'
 BIN_WC='/usr/bin/wc'
 CMD_WC='-l'
-BIN_GREP='/usr/bin/grep'
 BIN_ZYPPER='/usr/bin/zypper'
 CMD_ZYPPER='lu'
 BIN_YUM='/usr/bin/yum'
@@ -17,34 +22,40 @@ CMD_APT='-s upgrade'
 BIN_PACMAN='/usr/bin/pacman'
 CMD_PACMAN='-Sup'
 
-if [ -f $BIN_APT ]; then
-    # Debian / Ubuntu
-    UPDATES=`$BIN_APT $CMD_APT | $BIN_GREP 'Inst' | $BIN_WC $CMD_WC`
-    echo $UPDATES;
-elif [ -f $BIN_YUM ]; then
-    # CentOS / Redhat
-    UPDATES=`$BIN_YUM $CMD_YUM | $BIN_WC $CMD_WC`
-    if [ $UPDATES -gt 1 ]; then
-        echo $(($UPDATES-1));
-    else
-        echo "0";
-    fi
-elif [ -f $BIN_ZYPPER ]; then
-    # OpenSUSE
-    UPDATES=`$BIN_ZYPPER $CMD_ZYPPER | $BIN_WC $CMD_WC`
-    if [ $UPDATES -gt 3 ]; then
-        echo $(($UPDATES-3));
-    else
-        echo "0";
-    fi
-elif [ -f $BIN_PACMAN ]; then
-    # Arch
-    UPDATES=`$BIN_PACMAN $CMD_PACMAN | $BIN_WC $CMD_WC`
-    if [ $UPDATES -gt 1 ]; then
-        echo $(($UPDATES-1));
-    else
-        echo "0";
-    fi
+################################################################
+# Don't change anything unless you know what are you doing     #
+################################################################
+if [ -f /etc/os-release ]; then
+	OS=`$BIN_AWK -F= '/^ID=/{print $2}' /etc/os-release`
+	if [ $OS == "opensuse" ]; then
+		UPDATES=`$BIN_ZYPPER $CMD_ZYPPER | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 3 ]; then
+			echo $(($UPDATES-3));
+		else
+			echo "0";
+		fi
+	elif [ $OS == "\"centos\"" ]; then
+		UPDATES=`$BIN_YUM $CMD_YUM | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 6 ]; then
+			echo $(($UPDATES-6));
+		else
+			echo "0";
+		fi
+	elif [ $OS == "ubuntu" ]; then
+		UPDATES=`$BIN_APT $CMD_APT | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 1 ]; then
+			echo $(($UPDATES-1));
+		else
+				echo "0";
+		fi
+	elif [ $OS == "arch" ]; then
+		UPDATES=`$BIN_PACMAN $CMD_PACMAN | $BIN_WC $CMD_WC`
+		if [ $UPDATES -gt 1 ]; then
+        		echo $(($UPDATES-1));
+    		else
+        		echo "0";
+		fi
+	fi
 else
-    echo "0";
+	echo "0";
 fi
