3a46dfee176d640f512d460fcdefa218c4557208
Simon Mott <simon@simonmott.co.uk>
2017-03-22 02:43:30 +0000

    Rewrite ntp-server.sh agent script
    When running ntpd with a stratum 0 clock, some values from
    "ntpq -c rv" were missing.
    
    Namely the following:
    
    Local GPS clock: mintc=3, offset=-0.464 ...
    Syncing to remote NTP: tc=10, mintc=3, offset=-2.441 ...
    
    The lack of "tc=10" for local clock throws off the old for loop used
    to parse the output by 1 - so Frequnecy is used for offset and so on

--- snmp/ntp-server.sh
+++ snmp/ntp-server.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+# This script is deisigned to extract statistical information
+# from ntpd for use with LibreNMS.
+#
+# Tested on Rasbian 8 with ntp 4.2.6p5 but should work with newer versions
+#
+# Copyright (C) 2017 Simon Mott <me@simonmott.co.uk>
+#
+# Inspired by original libre-nms agent script by https://github.com/crcro
+
 ################################################################
 # copy this script to somewhere like /opt and make chmod +x it #
 # edit your snmpd.conf and include                             #
@@ -7,41 +17,87 @@
 # please make sure you have the path/binaries below            #
 ################################################################
 # Binaries and paths required                                  #
-################################################################ 
-BIN_NTPD='/usr/sbin/ntpd'
-BIN_NTPQ='/usr/sbin/ntpq'
-BIN_NTPDC='/usr/sbin/ntpdc'
-BIN_GREP='/usr/bin/grep'
-BIN_TR='/usr/bin/tr'
-BIN_CUT='/usr/bin/cut'
-BIN_SED='/usr/bin/sed'
 ################################################################
-# Don't change anything unless you know what are you doing     #
-################################################################
-VER=`$BIN_NTPD --version`
 
-CMD0=`$BIN_NTPQ -c rv | $BIN_GREP -Eow "stratum=[0-9]+" | $BIN_CUT -d "=" -f 2`
-echo $CMD0
+# Lets define some required binaries
+# These can be hard coded, but using "which" to make compatable with other distros
+BIN_NTPD=`which ntpd`
+BIN_NTPQ=`which ntpq`
+BIN_NTPDC=`which ntpdc`
+BIN_GREP=`which grep`
+BIN_CUT=`which cut`
+BIN_SED=`which sed`
+BIN_HEAD=`which head`
 
-CMD1=`$BIN_NTPQ -c rv | $BIN_GREP 'jitter' | $BIN_TR '\n' ' '`
-IFS=', ' read -r -a array <<< "$CMD1"
+# Below is a list of fields that LibreNMS currently expects from the poller and in which order
+#    'stratum'        => $stratum,
+#    'offset'         => $offset,
+#    'frequency'      => $frequency,
+#    'jitter'         => $jitter,
+#    'noise'          => $noise,
+#    'stability'      => $stability,
+#    'uptime'         => $uptime,
+#    'buffer_recv'    => $buffer_recv,
+#    'buffer_free'    => $buffer_free,
+#    'buffer_used'    => $buffer_used,
+#    'packets_drop'   => $packets_drop,
+#    'packets_ignore' => $packets_ignore,
+#    'packets_recv'   => $packets_recv,
+#    'packets_sent'   => $packets_sent,
 
-for value in 2 3 4 5 6
-do
-	echo ${array["$value"]} | $BIN_CUT -d "=" -f 2
-done
+# capture ntpq output
+NTPQ_OUTPUT=`$BIN_NTPQ -c rv`
 
-if [[ "$VER" =~ '4.2.6p5' ]]
-then
-  USECMD=`echo $BIN_NTPDC -c iostats`
+NTP_STRATUM=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "stratum=[0-9]+" | $BIN_CUT -d "=" -f 2`
+NTP_OFFSET=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "offset=(\-)?[0-9]+\.[0-9]+" | $BIN_CUT -d "=" -f 2`
+NTP_FREQUENCY=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "frequency=(\-)?[0-9]+\.[0-9]+" | $BIN_CUT -d "=" -f 2`
+NTP_JITTER=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "sys_jitter=(\-)?[0-9]+\.[0-9]+" | $BIN_CUT -d "=" -f 2`
+NTP_NOISE=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "clk_jitter=(\-)?[0-9]+\.[0-9]+" | $BIN_CUT -d "=" -f 2`
+NTP_STABILITY=`echo $NTPQ_OUTPUT | $BIN_GREP -Eow "clk_wander=(\-)?[0-9]+\.[0-9]+" | $BIN_CUT -d "=" -f 2`
+
+# ntp iostats
+
+# Old script looked for a specific version to indicate which command we should use
+# Lets do the same because I dont know any better :)
+NTP_VER=`echo $NTPQ_OUTPUT | $BIN_GREP -Eowc "version=.*4\.2\.6p5"`
+
+if [ $NTP_VER -eq 1 ]; then
+        # Version string contains 4.2.6p5...
+        NTP_IO_OUTPUT=`$BIN_NTPDC -c iostats`
 else
-  USECMD=`echo $BIN_NTPQ -c iostats localhost`
+        NTP_IO_OUTPUT=`$BIN_NTPQ -c iostats localhost`
 fi
-CMD2=`$USECMD | $BIN_TR -d ' ' | $BIN_TR '\n' ','`
 
-IFS=',' read -r -a array <<< "$CMD2"
+# Lets make the output easier to work with
+# bash has already formatted this for us so
+# there is a single space between : and the value
+#
+# So lets remove that space so we dont need
+# to worry about spaces later
+NTP_IO_OUTPUT=`echo $NTP_IO_OUTPUT | sed 's/: /:/g'`
 
-for value in 0 1 2 3 5 6 7 8
-do
-    echo ${array["$value"]} | $BIN_SED -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g'
-done
+NTP_UPTIME=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "time since reset:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_BUF_RX=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "receive buffers:[0-9]+" | $BIN_HEAD -1 | $BIN_CUT -d ":" -f 2`
+NTP_BUF_FREE=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "free receive buffers:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_BUF_USED=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "used receive buffers:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_PKT_DROP=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "dropped packets:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_PKT_IGN=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "ignored packets:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_PKT_RX=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "received packets:[0-9]+" | $BIN_CUT -d ":" -f 2`
+NTP_PKT_TX=`echo $NTP_IO_OUTPUT | $BIN_GREP -Eow "packets sent:[0-9]+" | $BIN_CUT -d ":" -f 2`
+
+# Do the needful
+
+echo $NTP_STRATUM
+echo $NTP_OFFSET
+echo $NTP_FREQUENCY
+echo $NTP_JITTER
+echo $NTP_NOISE
+echo $NTP_STABILITY
+echo $NTP_UPTIME
+echo $NTP_BUF_RX
+echo $NTP_BUF_FREE
+echo $NTP_BUF_USED
+echo $NTP_PKT_DROP
+echo $NTP_PKT_IGN
+echo $NTP_PKT_RX
+echo $NTP_PKT_TX
