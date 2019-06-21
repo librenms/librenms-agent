695bd34a393b8e49a0f76afc75f30d62058cf387
SniperVegeta <SniperVegeta@gmail.com>
2019-06-21 20:14:18 +0200

    Update and rename opcache to opcache.php
    Needs go true webserver not CLI.
    
    If some1 can break down https://github.com/gordalina/cachetool
    there via CLI it works, buts its an phar or so 
    dont know howto ...
    cant find a good example to connect to CGI socks

--- snmp/opcache.php
+++ snmp/opcache.php
@@ -1,10 +1,10 @@
 <?php
 
-# This is the status script that goes with the Cacti Templates for opcache via SNMP
 # It can be accessed as a snmpd extension with something like:
 #       extend phpopcache /usr/bin/curl --silent http://localhost/opcache.php
 # NOTE you should put an htaccess file on it for protection
 # need to be surved true the webserver for correct stats / CLI = other stats
+# TODO - If some sort of CGI connector to push true CLI for 1 file use....
 
 header('Content-Type: text/plain');
 $opcachestat = opcache_get_status ( False );
