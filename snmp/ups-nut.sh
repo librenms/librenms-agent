046e168586687fb1a38ca3d054211b7b57f4eef1
yon2004 <yon2004@gmail.com>
2020-05-09 02:39:08 +1000

    Added ups.status
    It's a space separated value so awk needed to change to print all values but the first. possible values are documented here.
    https://networkupstools.org/docs/developer-guide.chunked/ar01s04.html#_status_data

--- snmp/ups-nut.sh
+++ snmp/ups-nut.sh
@@ -14,9 +14,9 @@ UPS_NAME='APCUPS'
 PATH=$PATH:/usr/bin:/bin
 TMP=$(upsc $UPS_NAME 2>/dev/null)
 
-for value in "battery\.charge: [0-9.]+" "battery\.(runtime\.)?low: [0-9]+" "battery\.runtime: [0-9]+" "battery\.voltage: [0-9.]+" "battery\.voltage\.nominal: [0-9]+" "input\.voltage\.nominal: [0-9.]+" "input\.voltage: [0-9.]+" "ups\.load: [0-9.]+"
+for value in "battery\.charge: [0-9.]+" "battery\.(runtime\.)?low: [0-9]+" "battery\.runtime: [0-9]+" "battery\.voltage: [0-9.]+" "battery\.voltage\.nominal: [0-9]+" "input\.voltage\.nominal: [0-9.]+" "input\.voltage: [0-9.]+" "ups\.load: [0-9.]+" "ups\.status: [A-Z\ ]+"
 do
-	OUT=$(echo $TMP | grep -Eo "$value" | awk '{print $2}' | LANG=C sort | head -n 1)
+	OUT=$(echo $TMP | grep -Eo "$value" | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' | LANG=C sort | head -n 1)
 	if [ -n "$OUT" ]; then
 		echo $OUT
 	else
