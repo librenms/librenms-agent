1d071ff762455a409bb828e910afae8ab0ba71b6
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2020-06-02 16:23:09 +0200

    Optimize file using redis.py example

--- snmp/backupninja.py
+++ snmp/backupninja.py
@@ -1,49 +1,40 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import io
 import re
-import gzip
 import os
 import json
 
-LOGFILES = [
-    "/var/log/backupninja.log",
-]
+version = 1
+error = 0
+error_string = ''
 
-def main():
-    last_actions, last_fatal, last_error, last_warning = get_backupninja_state()
-    output = {'version': '1', 'error': '0', 'error_string': '', 'actions': last_actions, 'fatal': last_fatal, 'error': last_error, 'warning': last_warning}
-    print(json.dumps(output))
+logfile = '/var/log/backupninja.log'
 
+last_actions = 0
+last_fatal = 0
+last_error = 0
+last_warning = 0
 
-def readlog(logfile):
-    if logfile.endswith('.gz'):
-        return gzip.open(logfile,'r')
-    else:
-        return io.open(logfile,'r')
+if not os.path.isfile(logfile):
+    error_string = 'file unavailable'
+    break
 
+with io.open(logfile,'r') as f:
+    for line in reversed(list(f)):
+        match = re.search('^(.*) [a-zA-Z]*: FINISHED: ([0-9]+) actions run. ([0-9]+) fatal. ([0-9]+) error. ([0-9]+) warning.$', line)
+        if match:
+            last_actions = int(match.group(2))
+            last_fatal = int(match.group(3))
+            last_error = int(match.group(4))
+            last_warning = int(match.group(5))
+            break
 
-def get_backupninja_state():
-    last_actions = 0
-    last_fatal = 0
-    last_error = 0
-    last_warning = 0
-    
-    for logfile in LOGFILES:
-        if not os.path.isfile(logfile):
-            continue
+output = {'version': version,
+          'error': error,
+          'errorString': error_string,
+          'actions': last_actions,
+          'fatal': last_fatal,
+          'error': last_error,
+          'warning': last_warning}
 
-        with readlog(logfile) as f:
-            for line in reversed(list(f)):
-                match = re.search('^(.*) [a-zA-Z]*: FINISHED: ([0-9]+) actions run. ([0-9]+) fatal. ([0-9]+) error. ([0-9]+) warning.$', line)
-                if match:
-                    last_actions = int(match.group(2))
-                    last_fatal = int(match.group(3))
-                    last_error = int(match.group(4))
-                    last_warning = int(match.group(5))
-                    break
-    
-    return last_actions, last_fatal, last_error, last_warning
-
-
-if __name__ == '__main__':
-    main()
+print(json.dumps(output))
