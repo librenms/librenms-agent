fa57ad5249138dbbed0ebcaa7ee95d594428aa4f
Anael Mobilia <AnaelMobilia@users.noreply.github.com>
2020-06-02 16:29:47 +0200

    Properly use of the LibreNMS json format

--- snmp/backupninja.py
+++ snmp/backupninja.py
@@ -10,31 +10,30 @@ error_string = ''
 
 logfile = '/var/log/backupninja.log'
 
-last_actions = 0
-last_fatal = 0
-last_error = 0
-last_warning = 0
+backupninja_datas = {
+    'last_actions': 0,
+    'last_fatal': 0,
+    'last_error':  0,
+    'last_warning': 0}
 
 if not os.path.isfile(logfile):
     error_string = 'file unavailable'
+    error = 1
     break
 
 with io.open(logfile,'r') as f:
     for line in reversed(list(f)):
         match = re.search('^(.*) [a-zA-Z]*: FINISHED: ([0-9]+) actions run. ([0-9]+) fatal. ([0-9]+) error. ([0-9]+) warning.$', line)
         if match:
-            last_actions = int(match.group(2))
-            last_fatal = int(match.group(3))
-            last_error = int(match.group(4))
-            last_warning = int(match.group(5))
+            backupninja_datas['last_actions'] = int(match.group(2))
+            backupninja_datas['last_fatal'] = int(match.group(3))
+            backupninja_datas['last_error'] = int(match.group(4))
+            backupninja_datas['last_warning'] = int(match.group(5))
             break
 
 output = {'version': version,
           'error': error,
           'errorString': error_string,
-          'actions': last_actions,
-          'fatal': last_fatal,
-          'error': last_error,
-          'warning': last_warning}
+          'data': backupninja_datas}
 
 print(json.dumps(output))
