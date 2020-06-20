1228eb0b5e0866ed963501099c5249b2a94836b0
arrmo <github@rkmorris.us>
2020-06-20 14:02:49 -0500

    Scripts for Openwrt

---
+++ snmp/Openwrt/wlClients.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+ 
+# wlClients.sh
+# Counts connected (associated) Wi-Fi devices
+# Arguments: targed interface. Assumes all interfaces if no argument
+
+# Check number of arguments
+if [ $# -gt 1 ]; then
+	/bin/echo "Usage: wlClients.sh interface"
+	/bin/echo "Too many command line arguments, exiting."
+	exit 1
+fi
+
+# Get path to this script
+scriptdir=$(dirname $(readlink -f -- $0))
+
+# Get hostname, interface list. Set target, which is name returned for interface
+hostname=`/bin/uname -n`
+if [ $1 ]; then
+	interfaces=$1
+	target=$1
+else
+	interfaces=`cat $scriptdir/wlInterfaces.txt | cut -f 1 -d","`
+	target=wlan
+fi
+
+# Count associated devices
+count=0
+for interface in $interfaces
+do
+	new=`/usr/sbin/iw dev $interface station dump | /bin/grep Station | /usr/bin/cut -f 2 -s -d" " | /usr/bin/wc -l`
+  	count=$(( $count + $new ))
+done
+
+# Return snmp result
+/bin/echo $count
---
+++ snmp/Openwrt/wlNoiseFloor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ 
+# wlNoiseFloor.sh
+# Returns wlNoiseFloor, in dBm
+# Arguments: targed interface
+
+# Check number of arguments
+if [ $# -ne 1 ]; then
+	/bin/echo "Usage: wlNoiseFloor.sh interface"
+	/bin/echo "Missing targeted interface, exiting."
+	exit 1
+fi
+
+# Get hostname, extract noise floor. Note, all associated stations have the same value, so just grab the first one
+# Use tail, not head (i.e. last line, not first), as head exits immediately, breaks the pipe to cut!
+hostname=`/bin/uname -n`
+noise=`/usr/bin/iwinfo $1 assoclist | /usr/bin/cut -s -d "/" -f 2 | /usr/bin/cut -s -d "(" -f 1 | /usr/bin/cut -s -d " " -f 2 | /usr/bin/tail -1`
+
+# Return snmp result
+/bin/echo $noise
