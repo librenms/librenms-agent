c222f71e32363742bbc1c917b087fa190985143d
arrmo <arrmo@users.noreply.github.com>
2020-07-09 16:13:51 -0500

    Scripts for Openwrt (#314)

---
+++ snmp/Openwrt/wlFrequency.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ 
+# wlFrequency.sh
+# Returns wlFrequency, in MHz (not channel number)
+# Arguments: targed interface
+
+# Check number of arguments
+if [ $# -ne 1 ]; then
+	/bin/echo "Usage: wlFrequency.sh interface"
+	/bin/echo "Missing targeted interface, exiting."
+	exit 1
+fi
+
+# Get hostname, extract frequency
+hostname=`/bin/uname -n`
+frequency=`/usr/sbin/iw dev $1 info | /bin/grep channel | /usr/bin/cut -f 2- -s -d" " | /usr/bin/cut -f 2- -s -d"(" | /usr/bin/cut -f 1 -s -d" "`
+
+# Return snmp result
+/bin/echo $frequency
