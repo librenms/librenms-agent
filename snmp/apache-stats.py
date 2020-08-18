0bb2c15c780c8d13d17d41d9b875cb25dd077cf8
Hans Erasmus <erasmushans27@gmail.com>
2020-08-18 20:37:44 +0200

    Rewrite Apache SNMP Agent to Python3 (#326)
    * Updated to work with python3
    
    Thanks to @murrant this file is now python3 compatible.
    
    * Formatted for PEP8 specs
    
    * Updated except according to request
    
    * Updated according to request
    
    As requested [here](https://github.com/librenms/librenms/pull/12009) by SourceDoctor.

--- snmp/apache-stats.py
+++ snmp/apache-stats.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python
-# Copyright (C) 2009  Glen Pitt-Pladdy
+#!/usr/bin/python3
+# Copyright(C) 2009  Glen Pitt-Pladdy
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# of the License, or(at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,73 +17,69 @@
 #
 #
 #
-
-CACHETIME = 30
-CACHEFILE = '/var/cache/librenms/apache-snmp'
-
-# check for cache file newer CACHETIME seconds ago
 import os
 import time
-if os.path.isfile ( CACHEFILE ) \
-	and ( time.time() - os.stat ( CACHEFILE )[8] ) < CACHETIME:
-	# use cached data
-	f = open ( CACHEFILE, 'r' )
-	data = f.read()
-	f.close()
+import urllib.request
+
+cachetime = 30
+cachefile = '/var/cache/librenms/apache-snmp'
+
+# Check for a cache file newer than cachetime seconds ago
+
+if os.path.isfile(cachefile) and (time.time() - os.stat(cachefile)[8]) < cachetime:
+    # Use cached data
+    f = open(cachefile, 'r')
+    data = f.read()
+    f.close()
 else:
-	# grab the status URL (fresh data)
-	# need debian package python-urlgrabber
-	from urlgrabber import urlread
-	data = urlread ( 'http://localhost/server-status?auto',
-					user_agent = 'SNMP Apache Stats' )
-	# write file
-	f = open ( CACHEFILE+'.TMP.'+`os.getpid()`, 'w' )
-	f.write ( data )
-	f.close()
-	os.rename ( CACHEFILE+'.TMP.'+`os.getpid()`, CACHEFILE )
+    # Grab the status URL (fresh data), needs package urllib3
+    data = urllib.request.urlopen("http://localhost/server-status?auto").read().decode('UTF-8')
+    # Write file
+    f = open(cachefile+'.TMP.'+str(os.getpid()), 'w')
+    f.write(data)
+    f.close()
+    os.rename(cachefile+'.TMP.'+str(os.getpid()), cachefile)
 
 
 # dice up the data
-scoreboardkey = [ '_', 'S', 'R', 'W', 'K', 'D', 'C', 'L', 'G', 'I', '.' ]
+scoreboardkey = ['_', 'S', 'R', 'W', 'K', 'D', 'C', 'L', 'G', 'I', '.']
 params = {}
 for line in data.splitlines():
-	fields = line.split( ': ' )
-	if len(fields) <= 1:
-                continue  # "localhost" as first line cause out of index error
-	elif fields[0] == 'Scoreboard':
-		# count up the scoreboard into states
-		states = {}
-		for state in scoreboardkey:
-				states[state] = 0
-		for state in fields[1]:
-			states[state] += 1
-	elif fields[0] == 'Total kBytes':
-		# turn into base (byte) value
-		params[fields[0]] = int(fields[1])*1024
-	elif len(fields) > 1:
-		# just store everything else
-		params[fields[0]] = fields[1]
+    fields = line.split(': ')
+    if len(fields) <= 1:
+        continue  # "localhost" as first line causes out of index error
+    elif fields[0] == 'Scoreboard':
+        # count up the scoreboard into states
+        states = {}
+        for state in scoreboardkey:
+            states[state] = 0
+        for state in fields[1]:
+            states[state] += 1
+    elif fields[0] == 'Total kBytes':
+        # turn into base(byte) value
+        params[fields[0]] = int(fields[1])*1024
+    elif len(fields) > 1:
+        # just store everything else
+        params[fields[0]] = fields[1]
 
-# output the data in order (this is because some platforms don't have them all)
+# output the data in order(this is because some platforms don't have them all)
 dataorder = [
-	'Total Accesses',
-	'Total kBytes',
-	'CPULoad',
-	'Uptime',
-	'ReqPerSec',
-	'BytesPerSec',
-	'BytesPerReq',
-	'BusyWorkers',
-	'IdleWorkers'
+    'Total Accesses',
+    'Total kBytes',
+    'CPULoad',
+    'Uptime',
+    'ReqPerSec',
+    'BytesPerSec',
+    'BytesPerReq',
+    'BusyWorkers',
+    'IdleWorkers'
 ]
 for param in dataorder:
-	try:
-		print params[param]
-#                print param
-	except:	# not all Apache's have all stats
-		print 'U'
+    try:
+        print(params[param])
+    except KeyError:  # not all Apache's have all stats
+        print('U')
 
 # print the scoreboard
 for state in scoreboardkey:
-	print states[state]
-#	print state
+    print(states[state])
