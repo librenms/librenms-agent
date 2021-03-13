ce5f7ec97295ae29dded167aab8e65b81394d4fe
Jellyfrog <Jellyfrog@users.noreply.github.com>
2021-03-13 02:55:05 +0100

    Format with shellcheck

--- snmp/Openwrt/wlSNR.sh
+++ snmp/Openwrt/wlSNR.sh
@@ -14,17 +14,17 @@ if [ $# -ne 2 ]; then
 fi
 
 # Get hostname, calculate result. Sum just for debug, and return integer (safest / easiest)
-hostname=`/bin/uname -n`
-snrlist=`/usr/bin/iwinfo $1 assoclist | /usr/bin/cut -s -d "/" -f 2 | /usr/bin/cut -s -d "(" -f 2 | /usr/bin/cut -s -d " " -f 2 | /usr/bin/cut -s -d ")" -f 1`
+hostname=$(/bin/uname -n)
+snrlist=$(/usr/bin/iwinfo "$1" assoclist | /usr/bin/cut -s -d "/" -f 2 | /usr/bin/cut -s -d "(" -f 2 | /usr/bin/cut -s -d " " -f 2 | /usr/bin/cut -s -d ")" -f 1)
 if [ "$2" == "sum" ]; then
-  result=`/bin/echo "$snrlist" | /usr/bin/awk -F ':' '{sum += $1} END {printf "%d\n", sum}'`
+  result=$(/bin/echo "$snrlist" | /usr/bin/awk -F ':' '{sum += $1} END {printf "%d\n", sum}')
 elif [ "$2" == "avg" ]; then
-  result=`/bin/echo "$snrlist" | /usr/bin/awk -F ':' '{sum += $1} END {printf "%d\n", sum/NR}'`
+  result=$(/bin/echo "$snrlist" | /usr/bin/awk -F ':' '{sum += $1} END {printf "%d\n", sum/NR}')
 elif [ "$2" == "min" ]; then
-  result=`/bin/echo "$snrlist" | /usr/bin/awk -F ':' 'NR == 1 || $1 < min {min = $1} END {printf "%d\n", min}'`
+  result=$(/bin/echo "$snrlist" | /usr/bin/awk -F ':' 'NR == 1 || $1 < min {min = $1} END {printf "%d\n", min}')
 elif [ "$2" == "max" ]; then
-  result=`/bin/echo "$snrlist" | /usr/bin/awk -F ':' 'NR == 1 || $1 > max {max = $1} END {printf "%d\n", max}'`
+  result=$(/bin/echo "$snrlist" | /usr/bin/awk -F ':' 'NR == 1 || $1 > max {max = $1} END {printf "%d\n", max}')
 fi
 
 # Return snmp result
-echo $result
+echo "$result"
