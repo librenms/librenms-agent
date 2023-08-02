2c6ae8b659d2e7a4270f3f66bc7bc76fb8c4f49a
SourceDoctor <sourcehhdoctor@googlemail.com>
2023-08-02 23:21:39 +0200

    check for puppet v7 summary file (#485)

--- snmp/puppet_agent.py
+++ snmp/puppet_agent.py
@@ -21,6 +21,7 @@ CONFIGFILE = "/etc/snmp/puppet.json"
 
 
 summary_files = [
+    "/var/cache/puppet/public/last_run_summary.yaml",
     "/var/cache/puppet/state/last_run_summary.yaml",
     "/opt/puppetlabs/puppet/cache/state/last_run_summary.yaml",
     "/opt/puppetlabs/puppet/public/last_run_summary.yaml",
