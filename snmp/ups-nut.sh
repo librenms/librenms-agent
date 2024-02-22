5fdabfcf59cc230daea1e34c67921bef69b5211d
Peca Nesovanovic <59750439+Npeca75@users.noreply.github.com>
2024-02-22 23:18:24 +0100

    Add temperature readout
    expose Battery Temperature value

--- snmp/ups-nut.sh
+++ snmp/ups-nut.sh
@@ -39,3 +39,11 @@ do
     fi
 done
 
+for value in "ups\.temperature: [0-9.]+"; do
+    OUT=$(echo "$TMP" | grep -Eo "$value" | awk '{print $2}' | LANG=C sort | head -n 1)
+    if [ -n "$OUT" ]; then
+	echo "$OUT"
+    else
+	echo "Unknown"
+    fi
+done
