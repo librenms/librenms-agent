17a4bd748f89e8f878399df7536eec7e49dcd274
Kossusukka <kossusukka@kossulab.net>
2025-11-18 17:11:33 +0200

    Fixed code layout as Super-Linter suggested

--- snmp/i2pd-stats.py
+++ snmp/i2pd-stats.py
@@ -34,10 +34,11 @@ I2PC_PASS = "itoopie"
 # Do not change! Must match LibreNMS version
 JSONVER = "1"
 
+
 class I2PControl(object):
     """Talk to I2PControl API"""
 
-    def __init__(self, url, password='itoopie'):
+    def __init__(self, url, password):
         self.url = url
         self.password = password
         self._token = None
@@ -47,10 +48,17 @@ class I2PControl(object):
         """Cached authentication token"""
         if not self._token:
             try:
-                self._token = self.do_post(self.url,
-                    json.dumps({'id': 1, 'method': 'Authenticate',
-                        'params': {'API': 1, 'Password': self.password},
-                        'jsonrpc': '2.0'}))["result"]["Token"]
+                self._token = self.do_post(
+                    self.url,
+                    json.dumps(
+                        {
+                            "id": 1,
+                            "method": "Authenticate",
+                            "params": {"API": 1, "Password": self.password},
+                            "jsonrpc": "2.0",
+                        }
+                    ),
+                )["result"]["Token"]
             except KeyError:
                 post_error("1", "Invalid I2PControl password or token!")
                 exit(1)
@@ -60,8 +68,10 @@ class I2PControl(object):
         """HTTP(S) handler"""
         req = urllib.request.Request(url, data=data.encode())
         try:
-            with urllib.request.urlopen(req, context=ssl._create_unverified_context(), timeout=5) as f:
-                resp = f.read().decode('utf-8')
+            with urllib.request.urlopen(
+                req, context=ssl._create_unverified_context(), timeout=5
+            ) as f:
+                resp = f.read().decode("utf-8")
         except urllib.error.URLError:
             post_error("2", "Unable to connect I2PControl socket!")
             exit(1)
@@ -72,26 +82,33 @@ class I2PControl(object):
 
     def request(self, method, params):
         """Execute authenticated request"""
-        params['Token'] = self.token
-        return self.do_post(self.url, json.dumps(
-            {'id': 1, 'method': method, 'params': params, 'jsonrpc': '2.0'}))
+        params["Token"] = self.token
+        return self.do_post(
+            self.url,
+            json.dumps({"id": 1, "method": method, "params": params, "jsonrpc": "2.0"}),
+        )
+
 
 def post_error(code: str, message: str):
     """Post error code+message as JSON for LibreNMS"""
-    resp_err = { "data": "", "version": JSONVER, "error": code, "errorString": message }
+    resp_err = {"data": "", "version": JSONVER, "error": code, "errorString": message}
 
     print(json.dumps(resp_err))
 
+
 def main():
     # Craft JSON request for I2PC
-    JSON_REQUEST = json.loads('{ "i2p.router.uptime": "", "i2p.router.net.status": "", "i2p.router.net.bw.inbound.1s": "", "i2p.router.net.bw.inbound.15s": "", "i2p.router.net.bw.outbound.1s": "", "i2p.router.net.bw.outbound.15s": "", "i2p.router.net.tunnels.participating": "", "i2p.router.net.tunnels.successrate": "", "i2p.router.netdb.knownpeers": "", "i2p.router.netdb.activepeers": "", "i2p.router.net.total.received.bytes": "", "i2p.router.net.total.sent.bytes": "" }')
+    JSON_REQUEST = json.loads(
+        '{ "i2p.router.uptime": "", "i2p.router.net.status": "", "i2p.router.net.bw.inbound.1s": "", "i2p.router.net.bw.inbound.15s": "", "i2p.router.net.bw.outbound.1s": "", "i2p.router.net.bw.outbound.15s": "", "i2p.router.net.tunnels.participating": "", "i2p.router.net.tunnels.successrate": "", "i2p.router.netdb.knownpeers": "", "i2p.router.netdb.activepeers": "", "i2p.router.net.total.received.bytes": "", "i2p.router.net.total.sent.bytes": "" }'
+    )
 
     ctl = I2PControl(I2PC_URL, I2PC_PASS)
 
-    resp = ctl.request('RouterInfo', JSON_REQUEST)['result']
-    resp_full = { "data": resp, "version": JSONVER, "error": "0", "errorString": "" }
+    resp = ctl.request("RouterInfo", JSON_REQUEST)["result"]
+    resp_full = {"data": resp, "version": JSONVER, "error": "0", "errorString": ""}
 
     print(json.dumps(resp_full))
+
 
 if __name__ == "__main__":
     main()
